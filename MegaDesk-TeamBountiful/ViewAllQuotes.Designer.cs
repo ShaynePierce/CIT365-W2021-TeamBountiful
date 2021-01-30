66d3f328f8f5695fa7f47a358b4dbd6b4147a9f4
kevinsnielsen <forgeofatlas@gmail.com>
2021-01-30 13:42:01 -0800

    Added DataGridView and data loading

--- MegaDesk-TeamBountiful/ViewAllQuotes.Designer.cs
+++ MegaDesk-TeamBountiful/ViewAllQuotes.Designer.cs
@@ -29,38 +29,63 @@ namespace MegaDesk_TeamBountiful
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.ButtonMainMenu = new System.Windows.Forms.Button();
+            this.QuotesDataGridView = new System.Windows.Forms.DataGridView();
+            this.filerBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.QuotesDataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.filerBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // ButtonMainMenu
             // 
             this.ButtonMainMenu.DialogResult = System.Windows.Forms.DialogResult.Cancel;
             this.ButtonMainMenu.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ButtonMainMenu.Location = new System.Drawing.Point(534, 468);
-            this.ButtonMainMenu.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.ButtonMainMenu.Location = new System.Drawing.Point(475, 374);
+            this.ButtonMainMenu.Margin = new System.Windows.Forms.Padding(1);
             this.ButtonMainMenu.Name = "ButtonMainMenu";
-            this.ButtonMainMenu.Size = new System.Drawing.Size(207, 72);
+            this.ButtonMainMenu.Size = new System.Drawing.Size(184, 58);
             this.ButtonMainMenu.TabIndex = 5;
             this.ButtonMainMenu.Text = "&Main Menu";
             this.ButtonMainMenu.UseVisualStyleBackColor = true;
             this.ButtonMainMenu.Click += new System.EventHandler(this.ButtonMainMenu_Click);
             // 
+            // QuotesDataGridView
+            // 
+            this.QuotesDataGridView.AllowUserToAddRows = false;
+            this.QuotesDataGridView.AllowUserToDeleteRows = false;
+            this.QuotesDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.QuotesDataGridView.Location = new System.Drawing.Point(12, 12);
+            this.QuotesDataGridView.Name = "QuotesDataGridView";
+            this.QuotesDataGridView.ReadOnly = true;
+            this.QuotesDataGridView.RowHeadersWidth = 50;
+            this.QuotesDataGridView.RowTemplate.Height = 24;
+            this.QuotesDataGridView.Size = new System.Drawing.Size(647, 358);
+            this.QuotesDataGridView.TabIndex = 6;
+            // 
+            // filerBindingSource
+            // 
+            this.filerBindingSource.DataSource = typeof(MegaDesk_TeamBountiful.Filer);
+            // 
             // ViewAllQuotes
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::MegaDesk_TeamBountiful.Properties.Resources.desktop_resized;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.CancelButton = this.ButtonMainMenu;
-            this.ClientSize = new System.Drawing.Size(756, 555);
+            this.ClientSize = new System.Drawing.Size(672, 444);
             this.ControlBox = false;
+            this.Controls.Add(this.QuotesDataGridView);
             this.Controls.Add(this.ButtonMainMenu);
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "ViewAllQuotes";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "View All Quotes";
+            ((System.ComponentModel.ISupportInitialize)(this.QuotesDataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.filerBindingSource)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -68,5 +93,7 @@ namespace MegaDesk_TeamBountiful
         #endregion
 
         private System.Windows.Forms.Button ButtonMainMenu;
+        private System.Windows.Forms.DataGridView QuotesDataGridView;
+        private System.Windows.Forms.BindingSource filerBindingSource;
     }
 }
--- MegaDesk-TeamBountiful/ViewAllQuotes.cs
+++ MegaDesk-TeamBountiful/ViewAllQuotes.cs
@@ -8,6 +8,8 @@ namespace MegaDesk_TeamBountiful
         public ViewAllQuotes()
         {
             InitializeComponent();
+
+            InitalizeDataGrid();
         }
 
         private void ButtonMainMenu_Click(object sender, EventArgs e)
@@ -16,15 +18,64 @@ namespace MegaDesk_TeamBountiful
             Close();
         }
 
-        /*
-         
-        To view all.. iterate through Filer.DeskQuotes. Something like:
+        private void InitalizeDataGrid()
+        {
+            DataGridView quotesDataGridView = QuotesDataGridView;
 
-        foreach (var quote in MainMenu.DataFiler.DeskQuotes)
+            string[] headers =
             {
-                Do something with 'quote';            
-            }         
-         
-         */
+                "Customer Name",
+                "Width",
+                "Depth",
+                "Drawers",
+                "Surface Material",
+                "Rush Order",
+                "Date",
+                "Total Price",
+            };
+
+            // Force select column smaller
+            quotesDataGridView.RowHeadersWidth = 20;
+            // Set columns widths
+            int[] columnWidths = {
+                100,
+                40,
+                40,
+                55,
+                60,
+                50,
+                60,
+                55,
+            };
+
+            quotesDataGridView.ColumnCount = headers.Length;
+
+            for (int i = 0; i < headers.Length; i++)
+            {
+                quotesDataGridView.Columns[i].Name = headers[i];
+                quotesDataGridView.Columns[i].Width = columnWidths[i];
+            }
+
+            //QuotesDataGridView.Rows.Clear();
+
+            // To view all.. iterate through Filer.DeskQuotes. Something like:
+            foreach (var quote in MainMenu.DataFiler.DeskQuotes)
+            {
+                string[] row =
+                {
+                    quote.CustomerName,
+                    quote.TheDesk.Width.ToString(),
+                    quote.TheDesk.Depth.ToString(),
+                    quote.TheDesk.Drawers.ToString(),
+                    quote.TheDesk.SurfaceMaterial.ToString(),
+                    quote.RushOrder,
+                    quote.QuoteDate.ToString("d"),
+                    string.Format($"{quote.TotalPrice:C}"),
+                };
+
+                QuotesDataGridView.Rows.Add(row);
+            }   
+        }
+
     }
 }
