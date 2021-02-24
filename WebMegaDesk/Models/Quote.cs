cce11bcb4c3e2f65612cf0b24bba5a432ac7c9d9
kyle-b-johnson <johnson.kyleb@gmail.com>
2021-02-23 21:33:03 -0600

    Addition of constants

--- WebMegaDesk/Models/Quote.cs
+++ WebMegaDesk/Models/Quote.cs
@@ -7,7 +7,11 @@ namespace WebMegaDesk.Models
 {
     public class Quote
     {
-
+        /* Constants to avoid "Magic Numbers" */
+        public const double BASECOST = 200.00;
+        public const double MAX_AREA_FREE = 1000;
+        public const double EXTRA_AREA_COST = 1.00;
+        public const double DRAWER_COST = 50.00;
 
         public int ID { get; set; }
 
