853b999e7e9a4caff0b9bfd9737b37e273a3107f
Tom Knorr <kno19005@byui.edu>
2021-02-26 23:25:22 -0700

    Added missing migration.

---
+++ WebMegaDesk/Migrations/20210225181113_AddQuoteUpdates.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebMegaDesk.Migrations
+{
+    public partial class AddQuoteUpdates : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CustomerFullName",
+                table: "Quote");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "QuoteDate",
+                table: "Quote",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "QuoteDate",
+                table: "Quote");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CustomerFullName",
+                table: "Quote",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
